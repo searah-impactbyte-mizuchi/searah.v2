0e4620960911dfd26f8637d2db5d8395abf23502
Sarah Julia Gushef <sarahgushef@gmail.com>
2020-05-16 23:59:53 +0700

    Create redux for get user profile and update user profile feature

---
+++ src/redux/store.js
@@ -0,0 +1,17 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
+import allReducer from './reducers';
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+    allReducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
